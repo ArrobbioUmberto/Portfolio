a3e1c3d8921d4021338f55989b9d94d1718d607f
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-08-25 23:45:16 +0200

    Edit set img main

--- components/main/Main.jsx
+++ components/main/Main.jsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import classes from './Main.module.css';
+import Presentation from './Presentation';
 function Main() {
   return (
     <Fragment>
       <div className={classes.container}>
         <h1>Hero sfondo con mega scritta sopra bianca </h1>
       </div>
+      <Presentation />
     </Fragment>
   );
 }
