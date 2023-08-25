9a0fb909b00c7944a8ea272d1162242cc9e5feca
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-08-25 23:21:45 +0200

    Main edit + first Presentation

---
+++ components/main/Main.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import classes from './Main.module.css';
+function Main() {
+  return (
+    <Fragment>
+      <div className={classes.container}>
+        <h1>Hero sfondo con mega scritta sopra bianca </h1>
+      </div>
+    </Fragment>
+  );
+}
+
+export default Main;
