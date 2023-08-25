e2f46fef9ed1eca53354e4dd451c3a43acaff227
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-08-25 23:21:26 +0200

    Navbar edit

---
+++ components/header/Navbar.jsx
@@ -0,0 +1,29 @@
+'use client';
+import Link from 'next/link';
+import React from 'react';
+import classes from './Navbar.module.css';
+function Navbar() {
+  return (
+    <nav className={classes.navbar}>
+      <ul className={classes.list}>
+        <li>
+          <Link href={'/'} className={classes.item}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href={'/'} className={classes.item}>
+            Portfolio
+          </Link>
+        </li>
+        <li>
+          <Link href={'/'} className={classes.item}>
+            Contattami
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
