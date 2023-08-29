4e64612d5ad2b8c0985adf6f3b7b9a73a0de8132
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-08-29 20:43:13 +0200

    EDIT first style foote component and idea

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -5,7 +5,11 @@ import Link from 'next/link';
 function Footer() {
   return (
     <div className={classes.row}>
-      <div className={classes.col}>
+      <div className={classes.col_40}>
+        <h1>
+          SEZIONE CON CONTATTI, CV(SCARICABILE), ATTESTATI(VISUALIZZABILE),
+          SOCIAL(LOGHI CLICCABILI)
+        </h1>
         <ul className={classes.list}>
           <li className={classes.item}>
             <Link href={'./'}>Chi sono....</Link>
@@ -17,6 +21,9 @@ function Footer() {
             <Link href={'./'}>Tecnologie</Link>
           </li>
         </ul>
+      </div>
+      <div className={classes.col_60}>
+        <h1>FORM CON POSSIBILITÀ INVIO MESSAGGIO ALLA MIA MAIL PERSONALE</h1>
       </div>
     </div>
   );
