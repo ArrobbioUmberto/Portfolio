80d782b8b66af4748fa33bdc2193e45d40d54c11
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-08-31 14:50:40 +0200

    Set container and scroll down contattami

--- components/header/Navbar.jsx
+++ components/header/Navbar.jsx
@@ -4,25 +4,27 @@ import React from 'react';
 import classes from './Navbar.module.css';
 function Navbar() {
   return (
-    <nav className={classes.navbar}>
-      <ul className={classes.list}>
-        <li>
-          <Link href={'/'} className={classes.item}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href={'/portfolio'} className={classes.item}>
-            Portfolio
-          </Link>
-        </li>
-        <li>
-          <Link href={'/'} className={classes.item}>
-            Contattami
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <div className={classes.Nav_container}>
+      <nav className={classes.navbar}>
+        <ul className={classes.list}>
+          <li>
+            <Link href={'/'} className={classes.item}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href={'/portfolio'} className={classes.item}>
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link href={'#contatti'} className={classes.item}>
+              Contattami
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
