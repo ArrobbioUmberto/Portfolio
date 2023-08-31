5ff0dac521610c39ace30c3f452c9a405892b330
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-08-31 14:51:04 +0200

    Edit New hero component with loading

--- components/main/Hero.jsx
+++ components/main/Hero.jsx
@@ -1,16 +1,12 @@
 'use client';
-import React, { Fragment } from 'react';
-import classes from './Hero.module.css';
+import React, { Fragment, useEffect, useState } from 'react';
+import HeroBg from '../ui/HeroBg';
 
 // import trace from './componets/main/Main.module.css';
 function Hero() {
   return (
     <Fragment>
-      <div className={classes.container}>
-        <h1 className={classes.title}>
-          Hero sfondo con mega scritta sopra bianca
-        </h1>
-      </div>
+      <HeroBg />
     </Fragment>
   );
 }
---
+++ components/ui/HeroBg.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import classes from './HeroBg.module.css';
+import HeroLoading from '../ui/HeroLoading';
+function HeroBg() {
+  const text = [
+    'Ogni grande viaggio inizia con un piccolo passo.',
+    "Non importa quanto sia difficile, l'importante  ",
+    'è iniziare e perseverare con determinazione.',
+  ];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isLoaded, setIsLoaded] = useState(false);
+  // const [isAnimationStarted, setIsAnimationStarted] = useState(false);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoaded(true);
+      setCurrentIndex(1);
+    }, 0); // Delay
+    return () => clearTimeout(timeout);
+  }, []);
+  useEffect(() => {
+    if (isLoaded && currentIndex < text.length) {
+      const timeout = setTimeout(() => {
+        setCurrentIndex(currentIndex + 1);
+      }, 4000); // Delay in milliseconds for each line
+
+      return () => clearTimeout(timeout);
+    }
+  }, [currentIndex, isLoaded, text.length]);
+  return (
+    <>
+      {isLoaded ? (
+        <div className={classes.container}>
+          <div className={classes.title}>
+            {text.slice(0, currentIndex).map((line, index) => (
+              <span key={index} className={classes.animation}>
+                {line}
+                <br />
+              </span>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <HeroLoading />
+      )}
+    </>
+  );
+}
+
+export default HeroBg;
