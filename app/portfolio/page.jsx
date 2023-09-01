a395aac4db4f01bdf7636ce6bd8ff2899a3dac11
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-09-01 17:19:36 +0200

    new page portfolio

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -1,13 +1,21 @@
 'use client';
-
+import Navbar from '@/components/header/Navbar';
+import Footer from '@/components/footer/Footer';
 import React from 'react';
-
+import classes from './page.module.css';
+import Video from '@/components/portfolio/main/Video';
 function page() {
   return (
     <>
-      <div className='container'>
-        ELENCO DEI VARI LAVORI CON DESCRIZIONE VARIE
-      </div>
+      <header className={classes.header}>
+        <Navbar />
+      </header>
+      <main>
+        <Video />
+      </main>
+      <footer className={classes.footer}>
+        <Footer />
+      </footer>
     </>
   );
 }
