4b64d34cbbdb62f0b84b3a1f3efb7adb9b7dde10
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-09-01 19:19:19 +0200

    scroll to buttom click contattami add

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -4,19 +4,21 @@ import Footer from '@/components/footer/Footer';
 import React from 'react';
 import classes from './page.module.css';
 import Video from '@/components/portfolio/main/Video';
+import CardFolder from '@/components/portfolio/main/CardFolder';
 function page() {
   return (
-    <>
+    <div className={classes.container}>
       <header className={classes.header}>
         <Navbar />
       </header>
-      <main>
+      <main className={classes.main}>
         <Video />
+        <CardFolder />
       </main>
-      <footer className={classes.footer}>
+      <footer className={classes.footer} id='contatti'>
         <Footer />
       </footer>
-    </>
+    </div>
   );
 }
 
