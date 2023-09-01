7ecc9cca8201bd903f0b10f2d7adb1404fe2e3c7
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-09-01 19:33:28 +0200

    card edit

--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import ImageSlider from './ImageSlider';
 import classes from './Card.module.css';
-function Card() {
-  return <div>Card</div>;
+function Card(props) {
+  const { project } = props;
+  return (
+    <>
+      <div className={classes.row}>
+        <div className={`${classes.col_50} ${classes.container_img}`}>
+          <ImageSlider images={project.img} />
+        </div>
+        <div className={classes.col_50}>
+          <h1 className={classes.title}> {project.name}</h1>
+          <p className={classes.description}>{project.description}</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Card;
