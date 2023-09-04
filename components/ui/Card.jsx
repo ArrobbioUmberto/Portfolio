fa84f5206bba3b5f0122f4caa505d1e9e75bb3a3
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-09-04 18:50:46 +0200

    add logo of every project

--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -3,6 +3,11 @@ import ImageSlider from './ImageSlider';
 import classes from './Card.module.css';
 function Card(props) {
   const { project } = props;
+  const logos = [project];
+  console.log(logos, 'logos');
+  console.log(logos[0].id, 'try');
+  // console.log(props, 'props');
+
   return (
     <>
       <div className={classes.row}>
@@ -12,15 +17,20 @@ function Card(props) {
         <div className={classes.col_35}>
           <h1 className={classes.title}> {project.name}</h1>
           <p className={classes.description}>{project.description}</p>
-          {/* <div className={classes.logos}>
-            {project.keys(props).map(
-              photo(
-                <>
-                  <img src={photo.logo} alt='logo' />
-                </>
-              )
-            )}
-          </div> */}
+          <div className={classes.logos}>
+            {logos.map((item, index) => (
+              <>
+                {item.logo.map((foto) => (
+                  <img
+                    key={index}
+                    src={foto}
+                    alt='logo'
+                    className={classes.logo}
+                  />
+                ))}
+              </>
+            ))}
+          </div>
         </div>
       </div>
     </>
