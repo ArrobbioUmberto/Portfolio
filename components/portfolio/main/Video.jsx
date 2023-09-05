c6c78916023e02a87784024b5edea4a91f9769aa
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-09-05 14:03:32 +0200

    rollback video

--- components/portfolio/main/Video.jsx
+++ components/portfolio/main/Video.jsx
@@ -1,20 +1,11 @@
 'use client';
 import React from 'react';
-import ReactPlayer from 'react-player';
 import classes from './Video.module.css';
 function Video() {
   return (
-    // <video className={classes.video} autoPlay muted loop>
-    //   <source src='/videos/coding.mp4' type='video/mp4' alt='il video' />
-    // </video>
-
-    <ReactPlayer
-      url='/videos/coding.mp4'
-      autoplay
-      muted
-      loop
-      className={classes.video}
-    />
+    <video className={classes.video} autoPlay muted loop>
+      <source src='/videos/coding.mp4' type='video/mp4' alt='il video' />
+    </video>
   );
 }
 
