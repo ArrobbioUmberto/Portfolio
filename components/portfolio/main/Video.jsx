67a5ea0f242923306623d70d6a7af40c84923604
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-09-05 13:55:22 +0200

    try video edit

--- components/portfolio/main/Video.jsx
+++ components/portfolio/main/Video.jsx
@@ -1,11 +1,12 @@
-'use client';
 import React from 'react';
+import ReactPlayer from 'react-player';
 import classes from './Video.module.css';
 function Video() {
   return (
-    <video className={classes.video} autoPlay muted loop>
-      <source src='/videos/coding.mp4' type='video/mp4' alt='il video' />
-    </video>
+    // <video className={classes.video} autoPlay muted loop>
+    //   <source src='/videos/coding.mp4' type='video/mp4' alt='il video' />
+    // </video>
+    <ReactPlayer url='/video/coding.mp4' />
   );
 }
 
