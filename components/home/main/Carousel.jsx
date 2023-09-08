4874ab7e68e36236b263a1b9846426b845c45b74
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-09-08 19:47:07 +0200

    fix image carousel webp

--- components/home/main/Carousel.jsx
+++ components/home/main/Carousel.jsx
@@ -13,32 +13,32 @@ function Try() {
     {
       title: 'boolbnb',
       id: 1,
-      img: '/progetti/boolbnb/boolbnb-1.png',
+      img: '/progetti/boolbnb/boolbnb-1.webp',
     },
     {
       title: 'whatsapp',
       id: 2,
-      img: '/progetti/mono/whatsapp-1.png',
+      img: '/progetti/mono/whatsapp-1.webp',
     },
     {
       title: 'spotify',
       id: 3,
-      img: '/progetti/mono/spotify-pic.png',
+      img: '/progetti/mono/spotify-pic.webp',
     },
     {
       title: 'boolflix',
       id: 4,
-      img: '/progetti/boolflix/boolflix-1.png',
+      img: '/progetti/boolflix/boolflix-1.webp',
     },
     {
       title: 'dc-comics',
       id: 5,
-      img: '/progetti/dc-comics/dc-comics-1.png',
+      img: '/progetti/dc-comics/dc-comics-1.webp',
     },
     {
       title: 'everlead',
       id: 6,
-      img: '/progetti/everlead/everlead-1.png',
+      img: '/progetti/everlead/everlead-1.webp',
     },
   ];
   const imgRef = useRef(null);
--- components/home/main/Presentation.jsx
+++ components/home/main/Presentation.jsx
@@ -4,12 +4,76 @@ import Image from 'next/image';
 import classes from './Presentation.module.css';
 import CV from '../../../public/images/foto-cv-linkedin.jpg';
 function Presentation() {
+  const logos = [
+    {
+      name: 'bootstrap',
+      id: 1,
+      src: '/languages/bootstrap.svg',
+    },
+    {
+      name: 'css',
+      id: 2,
+      src: '/languages/css3.svg',
+    },
+    {
+      name: 'html',
+      id: 3,
+      src: '/languages/html5.svg',
+    },
+    {
+      name: 'js',
+      id: 4,
+      src: '/languages/js.svg',
+    },
+    {
+      name: 'laravel',
+      id: 5,
+      src: '/languages/laravel.svg',
+    },
+    {
+      name: 'php',
+      id: 6,
+      src: '/languages/php-logo.svg',
+    },
+    {
+      name: 'sass',
+      id: 7,
+      src: '/languages/sass.svg',
+    },
+    {
+      name: 'vuejs',
+      id: 8,
+      src: '/languages/vuejs.svg',
+    },
+    {
+      name: 'react',
+      id: 9,
+      src: '/languages/react.svg',
+    },
+    {
+      name: 'mongodb',
+      id: 10,
+      src: '/languages/mongodb.svg',
+    },
+    {
+      name: 'nextjs',
+      id: 11,
+      src: '/languages/nextjs.svg',
+    },
+  ];
+  
   return (
     <Fragment>
       <div className={classes.container}>
         <div className={classes.row}>
           <div className={classes.col_4}>
             <Image src={CV} alt='foto cv' className={classes.img} />
+            <div className={classes.mobile} >
+              <h2 className={classes.text_logo}>Linguaggi di programmazione e di mark-up</h2>
+              <div className={classes.wrapper_logo}>
+              {logos.map((item)=><img className={classes.logo} src={item.src} key={item.id} alt={item.name}></img>)}
+              </div>
+            </div>
           </div>
           <div className={classes.col_6}>
             <div className={classes.box_content}>
