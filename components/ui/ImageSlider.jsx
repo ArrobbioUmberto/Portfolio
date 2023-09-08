6e0d7c2e3ea745e38bbdbb83e50c344e1c4adb3e
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-09-08 18:01:22 +0200

    web speed improvment

--- components/ui/ImageSlider.jsx
+++ components/ui/ImageSlider.jsx
@@ -1,8 +1,10 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState,useEffect } from 'react';
 import classes from './ImageSlider.module.css';
 function ImageSlider(props) {
   const { images } = props;
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [imageVisible, setImageVisible] = useState(false);
+  const imgRef = useRef(null);
   let hideArrow = true;
   if (images.length === 1) {
     hideArrow = false;
@@ -22,9 +24,29 @@ function ImageSlider(props) {
       prevIndex === images.length - 1 ? 0 : prevIndex + 1
     );
   };
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setImageVisible(true);
+          observer.disconnect(); // Smetti di osservare l'elemento una volta che è visibile.
+        }
+      });
+    });
+
+    if (imgRef.current) {
+      observer.observe(imgRef.current);
+    }
+
+    return () => {
+      if (imgRef.current) {
+        observer.unobserve(imgRef.current);
+      }
+    };
+  }, []);
   return (
     <>
-      <img src={images[currentIndex]} alt='ciao' className={classes.img} />
+      <img ref={imgRef} src={imageVisible? images[currentIndex]: '/social/loading.svg'} alt='ciao' className={classes.img} />
       {hideArrow && (
         <>
           <button onClick={prevImage} className={classes.button}>
