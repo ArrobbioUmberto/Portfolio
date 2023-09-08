b179c17dfdebaa88dae6cc379fd6331003cbc5d2
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-09-08 16:18:05 +0200

    fix heigth on portfolio page

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -5,9 +5,16 @@ import Footer from '@/components/footer/Footer';
 import classes from './page.module.css';
 import Video from '@/components/portfolio/main/Video';
 import CardFolder from '@/components/portfolio/main/CardFolder';
-import React, { useRef } from 'react';
+import React, { useRef,useEffect } from 'react';
 function page() {
   const contattiRef = useRef(null);
+  useEffect(() => {
+    // Calcola l'altezza del viewport in unità vh
+    const vh = window.innerHeight * 0.01;
+
+    // Imposta il valore nella variabile CSS personalizzata --vh
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, [])
   return (
     <div className={classes.container}>
       <header className={classes.header}>
