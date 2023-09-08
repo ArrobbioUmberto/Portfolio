aef3ccaeda2848ab1125c26c3cb4c26ecdfb44a6
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-09-08 21:12:41 +0200

    iphome x

--- components/ui/ImageSlider.jsx
+++ components/ui/ImageSlider.jsx
@@ -45,7 +45,7 @@ function ImageSlider(props) {
     };
   }, []);
   return (
-    <div>
+    <div className={classes.wrapper}>
       <img ref={imgRef} src={imageVisible? images[currentIndex]: '/social/loading.svg'} alt='ciao' className={classes.img} />
       {hideArrow && (
         <>
