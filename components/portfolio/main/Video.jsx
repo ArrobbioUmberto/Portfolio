ec7f767e45ac4a07f4f2f94ec2181aed95d717e4
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-09-09 12:28:16 +0200

    video fix for mobile

--- components/portfolio/main/Video.jsx
+++ components/portfolio/main/Video.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 import classes from './Video.module.css';
 function Video() {
   return (
-    <video className={classes.video} autoPlay muted loop>
+    <video className={classes.video} autoPlay={true} 
+    loop={true}
+    controls={false} 
+    playsInline
+    muted >
       <source src='/videos/coding.mp4' type='video/mp4' alt='il video' />
     </video>
   );
